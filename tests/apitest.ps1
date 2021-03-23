915f7f56e276e89cc167a0f367f93d3b8db27231
phil <phil@intellitect.com>
2021-03-23 06:30:39 -0700

    update tests

--- tests/apitest.ps1
+++ tests/apitest.ps1
@@ -3,14 +3,18 @@ $RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantId/oauth2/tok
 $body = "grant_type=client_credentials&client_id=$ClientId&client_secret=$ClientSecret&resource=$Resource"
 $Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body -ContentType 'application/x-www-form-urlencoded'
 
-# Write-Output "Token: "; $Token
+Write-Output "Token: "; $Token
 
 # # Hit website
-# $SiteUri = "https://samplecrm-myrapp-test.azurewebsites.net/"
+#$SiteUri = "https://samplecrm-myrapp-test.azurewebsites.net"
+$SiteUri = $Resource
+Write-Output "Site: "; $SiteUri
 
-# $Headers = @{}
-# $Headers.Add("Authorization","$($Token.token_type) "+ " " + "$($Token.access_token)")
-# $site = Invoke-RestMethod -Method Get -Uri $SiteUri -Headers $Headers
+$Headers = @{}
+$Headers.Add("Authorization","$($Token.token_type)"+ " " + "$($Token.access_token)")
+$Headers.Add("Accept", "*/*")
+$Headers.Add("Accept-Encoding", "gzip, deflate, br")
+$Headers
 
-# Write-Output $site
+$result = Invoke-RestMethod -Method Get -Uri $SiteUri -Headers $Headers
 
